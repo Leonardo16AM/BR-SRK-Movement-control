fb6431e906b2aec030313985c023e61f5f5e5098
Leonardo Daniel Artiles Montero <51642689+LeonardoArtiles16@users.noreply.github.com>
2021-08-03 18:52:58 -0400

    Added threads for every movement to fix crash

--- web_app.py
+++ web_app.py
@@ -1,26 +1,70 @@
 from flask import Flask,request,session,render_template
-import asyncio
 import movement_control as car
-
+import threading
+import time
 
 app=Flask(__name__)
 
+
+class Thread (threading.Thread):
+    def __init__(self,ch):
+        threading.Thread.__init__(self)
+        self.ch=ch
+    def run(self):
+        if(self.ch=='W'):
+            car.pulsa_w()
+            
+        if(self.ch=='A'):
+            car.pulsa_a()
+            
+        if(self.ch=='S'):
+            car.pulsa_s()
+            
+        if(self.ch=='D'):
+            car.pulsa_d()
+
+        if(self.ch=='Auto'):
+            car.pulsa_e()
+        while 1:pass
+        return
+
+
+
+big_thread=123
+
 @app.route('/',methods=['GET','POST'])
 def index():
+    global big_thread
     if request.method=='GET':    
+        print(threading.activeCount() )
+        print(threading.currentThread() )
+        print(threading.enumerate())
         return render_template('index.html')
     else:
-        print(request.form)
         if request.form.get('w')=='W':
-            car.pulsa_w()
+            big_thread=Thread('W')
+            big_thread.start()
+            return render_template('index.html')
+
         elif request.form.get('a')=='A':
-            car.pulsa_a()
+            big_thread=Thread('A')
+            big_thread.start()
+            return render_template('index.html')
+
         elif request.form.get('s')=='S':
-            car.pulsa_s()
+            big_thread=Thread('D')
+            big_thread.start()
+            return render_template('index.html')
+
         elif request.form.get('d')=='D':
-            car.pulsa_d()
+            big_thread=Thread('D')
+            big_thread.start()
+            return render_template('index.html')
+
         elif request.form.get('auto')=='Automatic':
-            car.pulsa_e()
+            big_thread=Thread('Auto')
+            big_thread.start()
+            return render_template('index.html')
 
         return render_template('index.html')
 
